c21a7537db5dbc2a997410bcb8a2cd25d2df5e03
Bee Clark <mrhumanman2@gmail.com>
2022-02-27 16:52:43 -0500

    Remove unneeded newline in Vars

--- src/routes/vars.jsx
+++ src/routes/vars.jsx
@@ -293,7 +293,6 @@ export default function Vars(props) {
 			setIsTable(true);
 			setNewName(name);
 			setNewScope(key === "global" ? "" : key);
-
 			setTableData([...vars[key][name]]);
 		}
 	};
