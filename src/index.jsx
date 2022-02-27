432e41849248edcf712cbaf6c5eb85b6e7272b66
Bee <mrhumanman2@gmail.com>
2022-02-26 23:02:26 -0500

    Basic math expressions

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import PouchDB from "pouchdb";
 import PouchDBFind from "pouchdb-find";
+import Handlebars from "handlebars/dist/handlebars.min.js";
 
 import App from "./routes/app";
 import Creator from "./routes/creator";
@@ -13,6 +14,87 @@ import Guides from "./routes/guides";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-notifications/lib/notifications.css";
+
+Handlebars.registerHelper("gt", (arg1, arg2) => {
+	return parseFloat(arg1) > parseFloat(arg2);
+});
+
+Handlebars.registerHelper("lt", (arg1, arg2) => {
+	return parseFloat(arg1) < parseFloat(arg2);
+});
+
+Handlebars.registerHelper("eq", (arg1, arg2) => {
+	// eslint-disable-next-line eqeqeq
+	return arg1 == arg2;
+});
+
+Handlebars.registerHelper("add", (arg1, arg2) => {
+	arg1 = parseFloat(arg1);
+	arg2 = parseFloat(arg2);
+
+	if (Number.isInteger(arg1)) {
+		arg1 = Math.floor(arg1);
+	}
+
+	if (Number.isInteger(arg2)) {
+		arg2 = Math.floor(arg2);
+	}
+
+	return arg1 + arg2;
+});
+
+Handlebars.registerHelper("sub", (arg1, arg2) => {
+	arg1 = parseFloat(arg1);
+	arg2 = parseFloat(arg2);
+
+	if (Number.isInteger(arg1)) {
+		arg1 = Math.floor(arg1);
+	}
+
+	if (Number.isInteger(arg2)) {
+		arg2 = Math.floor(arg2);
+	}
+
+	return arg1 - arg2;
+});
+
+Handlebars.registerHelper("mul", (arg1, arg2) => {
+	arg1 = parseFloat(arg1);
+	arg2 = parseFloat(arg2);
+
+	if (Number.isInteger(arg1)) {
+		arg1 = Math.floor(arg1);
+	}
+
+	if (Number.isInteger(arg2)) {
+		arg2 = Math.floor(arg2);
+	}
+
+	return arg1 * arg2;
+});
+
+Handlebars.registerHelper("div", (arg1, arg2) => {
+	arg1 = parseFloat(arg1);
+	arg2 = parseFloat(arg2);
+
+	if (Number.isInteger(arg1)) {
+		arg1 = Math.floor(arg1);
+	}
+
+	if (Number.isInteger(arg2)) {
+		arg2 = Math.floor(arg2);
+	}
+
+	return arg1 / arg2;
+});
+
+Handlebars.registerHelper("floor", (arg) => {
+	return Math.floor(parseFloat(arg));
+});
+
+Handlebars.registerHelper("ceil", (arg) => {
+	return Math.ceil(parseFloat(arg));
+});
 
 PouchDB.plugin(PouchDBFind);
 
